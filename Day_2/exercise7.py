91e8bca5420c0fc24099ec2be902670ecaaafbc0
Blaben <blabensolutions@gmail.com>
2025-06-21 13:43:48 +0000

    corrected the code

--- Day_2/exercise7.py
+++ Day_2/exercise7.py
@@ -1,8 +1,8 @@
 #The Car Game
-x=0
-started = False
+started = False #this line checks the status of the car
+print("Enter start to see the list of available options ")
 #Instructions
-while x<1:
+while True: #this condition is set to make the loop run infinitely
     user_input = input("> ").upper()
     if user_input == "HELP":
         print("""
@@ -13,11 +13,19 @@ quit - To exit
     elif user_input == "START":
         #This line checks to make sure start is not repeated twice
         if started:
-            print("HeY!! You have already started the car")
+            print("Hey!! You have already started the car")
         else:
-            print("Car Started")
+            started = True
+            print("Car Started..")
+    
     elif user_input == "STOP":
-        print("Car stopped")
+        if not started:
+            print("Hey! you have already stopped the car..")
+
+        else:
+            started = False
+            print("Car stopped")
+
     elif user_input == "QUIT":
         break
     else:
